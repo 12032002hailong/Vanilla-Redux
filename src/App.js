8a20d66bcada7cc28c900ee7e425a1893db952d5
Hai Long <hailongkc1203@gmail.com>
2023-02-22 16:23:53 +0700

    Design Lyaout

--- src/App.js
+++ src/App.js
@@ -5,50 +5,63 @@ import {
   increaseCounter,
   decreaseCounter,
 } from "./action/actions";
-import store from './redux/store';
+import axios from 'axios';
+
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import Home from './components/Home';
 
 
-function App(props) {
+const App = (props) => {
+  // == const mapStateToProps = state => {
+  //   return {
+  //     count: state.counter.count,
+  //   }
+  // }
+  const dispatch = useDispatch();
+  const newCount = useSelector(
+    (state) => { return state.counter.count }
+  )
+
 
   const handleIncrease = () => {
+    // == const mapDispatchToProps = dispatch => {
+    //   return {
+    //     increaseCounter: () => dispatch(increaseCounter()),
 
-    // dispatch actions
-    props.increaseCounter()
-
-    //fire actions: dispatch = fire
-    // store.dispatch({
-    //   type: 'test by Hai Long',
-    //   payload: { 'name': "Hai Long" }
-    // })
+    //     decreaseCounter: () => dispatch(decreaseCounter()),
+    //   }
+    // }
+    // export default connect(mapStateToProps, mapDispatchToProps)
+    dispatch(increaseCounter());
   }
+
+  const fetchAllUser = async () => {
+    const res = await axios.get("http://localhost:8080/users/all");
+    const data = res && res.data ? res.data : [];
+    console.log("check res: ", data);
+  }
+
+  useEffect(() => {
+    fetchAllUser();
+  }, [])
+
+
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <h1>Hello world with React</h1>
+    //     <div>Count: {newCount}</div>
 
+    //     <button onClick={() => handleIncrease()}>Increase Count</button>
 
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React</h1>
-        <div>Count: {props.count}</div>
-
-        <button onClick={() => handleIncrease()}>Increase Count</button>
-
-        <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
-      </header>
-    </div>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
+    //   </header>
+    // </div>
+    <Home />
   );
 }
-const mapStateToProps = state => {
-  return {
-    count: state.counter.count,
-  }
-}
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
-
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+// export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import FormAddNew from './FormAddNew';
+import TableUser from './TableUser';
+
+
+const Home = (props) => {
+    return (
+        <>
+            <Header />
+            <FormAddNew />
+            <TableUser />
+        </>
+    )
+}
+
+export default Home
